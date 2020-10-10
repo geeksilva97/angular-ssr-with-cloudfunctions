d0bf606dff9680ec27bd52edce841c81306c3522
Edigleysson Silva <edigleyssonsilva@gmail.com>
2020-10-09 23:47:58 -0300

    build

--- functions/dist/out-tsc/server.js
+++ functions/dist/out-tsc/server.js
@@ -8,7 +8,7 @@ import { existsSync } from 'fs';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
     const server = express();
-    const distFolder = join(process.cwd(), 'dist/angular-serverside/browser');
+    const distFolder = join(process.cwd(), 'dist/browser');
     const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
     // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
     server.engine('html', ngExpressEngine({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,7 +18,9 @@ export class HomeComponent implements OnInit {
     this.metaTags.addTags([
       { name: 'title', content: 'This is my home page' },
       { name: 'description', content: 'This is my home page' },
-      { name: 'twitter:card', content: 'This is my home page' },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@edigleyssonsilva' },
+      { name: 'twitter:creator', content: '@edigleyssonsilva' },
       { name: 'og:url', content: '/home' },
       { name: 'og:title', content: 'Home Page' },
       { name: 'og:description', content: 'This is my home page' },
@@ -26,3 +28,13 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
+/**
+ * <meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@nytimesbits" />
+<meta name="twitter:creator" content="@nickbilton" />
+<meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
+<meta property="og:title" content="A Twitter for My Sister" />
+<meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
+<meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
+ */
